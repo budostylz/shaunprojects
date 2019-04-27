0999ec6b9d9a426c61bdeca51fab897fde45014f
budostylz <lewis.shaun1@outlook.com>
2019-04-27 19:57:05 -0400

    'commit'

--- README.md
+++
@@ -1,8 +0,0 @@
-# Shaun's Portfolio Site
-
-http://shaunprojects.shaunlewisprojects.com/index.html
-
-## npm_prompt.lua:11: attempt to concatenate local 'package_version' (a nil value)
-
-https://github.com/cmderdev/cmder/issues/700
-
---
+++ js/userPosition.js
@@ -0,0 +1,25 @@
+﻿/*
+    Display's map via user's position
+    Contributor: Shaun Lewis
+    4/17/2016
+
+*/
+(function () {
+    jQuery.noConflict();//let native libraries use global object
+    var iframe = jQuery("#position iframe");
+    //default view
+    iframe.attr("src", "https://www.google.com/maps/embed/v1/view?key=AIzaSyDlq7vkWkeVr5UfnvKdSAqhDU3MppO-Y84&center=37.0362625,-76.4736602&zoom=18&maptype=satellite");
+    getUserLocation();
+    function getUserLocation() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(getUserPosition);
+        } else {
+            x.innerHTML = "Geolocation is not supported by this browser.";
+        }
+    }//getUserLocation()
+    function getUserPosition(pos) {
+        var lat = pos.coords.latitude;
+        var long = pos.coords.longitude;
+        iframe.attr("src", "https://www.google.com/maps/embed/v1/view?key=AIzaSyDlq7vkWkeVr5UfnvKdSAqhDU3MppO-Y84&center="+lat+","+long+"&zoom=18&maptype=satellite");
+    } //getUserPosition(pos)
+})();
